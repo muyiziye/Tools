bdeb0efdbd1ceb5053c15b9be1dab3c7d2e3724c
muyiziye <348610826@qq.com>
2016-01-07 12:17:34 +0800

    add assert and debug

---
+++ lib/my_assert.h
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: my_assert.h
+	> Author: muyiziye
+	> Mail: 348610826@qq.com 
+	> Created Time: Thu 07 Jan 2016 10:57:28 AM CST
+ ************************************************************************/
+#ifndef _MY_ASSERT_H_
+#define _MY_ASSERT_H_
+
+#include <stdio.h>
+//#define NDEBUG
+#include <assert.h>
+
+#define MY_ASSERT(_bool) assert(_bool)
+
+#endif
---
+++ lib/my_debug.h
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: my_debug.h
+	> Author: muyiziye
+	> Mail: 348610826@qq.com 
+	> Created Time: Thu 07 Jan 2016 12:07:34 PM CST
+ ************************************************************************/
+
+#ifndef _MY_DEBUG_H_
+#define _MY_DEBUG_H_
+
+#define DEBUG
+#ifdef DEBUG
+//#include
+#define debug(fmt, x...) \
+	do \
+{\
+	printf("%s %s(Line %d):"fmt,__FILE__, __FUNCTION__, __LINE__,##x);\
+}while(0)
+#else
+#define debug(...)
+#endif
+
+#endif
