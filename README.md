9a385826ad17f7e285d37ceecfa8505d7fcf5da8
ly <ly@qq.com>
2019-04-12 10:07:16 +0800

    add my_ssh.sh to help login ssh servers

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Tools
-this project is my tools
+Some small tools
+
+# For bash tool
+1. my_ssh.sh
+    This is a small tool which can help to login to commonly used servers.
+
+2. opengoll.sh
+    Only for start gollum just work on my server.
+
---
+++ bash_tool/my_ssh.sh
@@ -0,0 +1,37 @@
+#########################################################################
+# File Name: my_ssh.sh
+# Author: liuyang
+# mail: liuyang91@lenovo.com
+# Created Time: Fri 12 Apr 2019 09:50:22 AM CST
+#########################################################################
+#!/bin/bash
+
+a="root@xx.xx.xx.xx"
+
+usage="$ `basename $0`  <option> \n
+                                                        \n
+Options:                                                \n
+-a \t\t ssh $a(login my ubuntu server)\n
+-h --help\t\thelp message.          \n
+"
+
+## handle parameters
+OPTS=`getopt -o abcdefgijklmnopqrstuvwxyzh -l help -- "$@"`
+eval set -- "$OPTS"
+while true; do
+    case $1 in
+        -a)
+            ssh $a; exit 0;        shift; continue;;
+        -h|--help)
+            echo -ne $usage ; exit 0;;
+        --)
+            break ;;
+        *)
+            printf "Unkonwn option %s\n" "$1";
+            break
+    esac
+done
+
+if [ $# -ne 0 ]; then
+    echo -ne $usage; exit 1
+fi
